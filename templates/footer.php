23a06f43ebeedfe6505e6ee686e545257976ca76
Eric Lemonne <eric@c50.be>
2018-08-24 05:01:35 +0200

    projinit-1.0 add prod theme

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,19 @@
-<footer class="content-info" role="contentinfo">
+<footer class="footer content-info" role="contentinfo">
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
-    <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+    <p><img alt="Projinit small logo" src="/wp-content/themes/projinit_roots/assets/img/projinit-hands.png">&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>
+      <span class="footerAdr">Ravenstraat 10, Buggenhout, East-Flanders 9255 Belgium, <nobr>VAT BE0864 657 802</nobr></span>
+    <div class="socialLogos">
+    <a class="fcbk_share" href="http://www.facebook.com/114040605277817" target="blank" title="Join us on Facebook!">
+      Follow Us on facebook
+	  </a>
+    <a class="twitt_share" href="https://twitter.com/projinit" target="_blank" title="Follow us on Twitter!">
+		Follow Us on twitter
+	  </a>
+    <a class="li_share" href="http://www.linkedin.com/company/206930" target="_blank" title="Join us on LinkedIn!">
+    Follow Us on LinkedIn
+    </a>
+    </div>
   </div>
 </footer>
 
